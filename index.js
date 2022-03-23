8597d1b4d5ca1ba5b4430371f9e11f4929fa0153
Duc La <laminhduc10@yahoo.com>
2022-03-22 18:35:00 -0700

    commit tree e28b778832fc873b62398b9507b973619177c10d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrayOfstringName, evenName){
+    const newArray = [];
+    for(let i = 0; i < arrayOfstringName.length; i++){
+        newArray.push(`Thank you, ${arrayOfstringName[i]}, for the wonderful ${evenName} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number = number-1;
+    }
+}
